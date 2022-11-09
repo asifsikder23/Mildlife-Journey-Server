bb918c3daf5f3a224788167fb8f1f6eee2b77f2d
Md. Asif Ullah Sikder <asifsikder23@gmail.com>
2022-11-10 03:29:11 +0600

    content added

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json())
 require('dotenv').config()
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORD_DB}@cluster0.0iyuemt.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 console.log(uri);
@@ -16,6 +16,31 @@ console.log(uri);
 async function run(){
     client.connect()
     console.log("database connected");
+    const ServiceCollection = client.db('mildlife-journey').collection('services')
+    app.get('/services', async(req, res)=>{
+      const query = {}
+      const result = await ServiceCollection.find(query).toArray();
+      res.send(result)
+    })
+
+    app.get('/limit', async(req, res)=>{
+      const query = {}
+      const result = await ServiceCollection.find(query).limit(3).toArray();
+      res.send(result)
+    })
+
+    app.get('/services/:id', async(req, res)=>{
+      const id = req.params.id
+      const query = {_id:ObjectId(id)}
+      const result = await ServiceCollection.findOne(query);
+      res.send(result)
+    })
+
+    app.post('/services', async(req, res)=>{
+      const query = req.body
+      const result = await ServiceCollection.insertOne(query);
+      res.send(result)
+    })
 }
 run()
 
