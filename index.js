ab6e5f487e12887f6f38a111bd51593e2d195a96
Md. Asif Ullah Sikder <asifsikder23@gmail.com>
2022-11-10 22:09:46 +0600

    content added

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ async function run(){
     client.connect()
     console.log("database connected");
     const ServiceCollection = client.db('mildlife-journey').collection('services')
+    const ReviewCollection = client.db('mildlife-journey').collection('review')
     app.get('/services', async(req, res)=>{
       const query = {}
       const result = await ServiceCollection.find(query).toArray();
@@ -35,12 +36,61 @@ async function run(){
       const result = await ServiceCollection.findOne(query);
       res.send(result)
     })
+    app.get('/review', async (req, res) => {
+      let query = {};
+
+      if (req.query.email) {
+          query = {
+              email: req.query.email
+          }
+      }
+      const cursor = ReviewCollection.find(query);
+      const orders = await cursor.sort({ $natural: -1 }).toArray();
+      console.log(orders);
+      res.send(orders);
+  });
 
     app.post('/services', async(req, res)=>{
       const query = req.body
       const result = await ServiceCollection.insertOne(query);
       res.send(result)
     })
+    app.post('/review', async(req, res)=>{
+      const query = req.body
+      const result = await ReviewCollection.insertOne(query);
+      res.send(result)
+    })
+
+    app.get('/review', async(req, res)=>{
+      const query = {}
+      const cursor = ReviewCollection.find(query);
+      const userReview = await cursor.toArray();
+      res.send(userReview);
+    });
+
+    app.delete('/reviews/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id:ObjectId(id)}
+      console.log('trying to delete', id);
+      const result = await ReviewCollection.deleteOne(query);
+      console.log(result);
+      res.send(result)
+  })
+  app.put('/reviews/:id', async(req, res)=>{
+    const id = req.params.id;
+    const filter = { _id: ObjectId(id) };
+    const user =  req.body;
+    const option = {upsert: true}
+    const updatedUser ={
+        $set:{
+            rating: user.rating,
+            message: user.massage,
+        }
+    }
+    console.log(updatedUser);
+    const result = await ReviewCollection.updateOne(filter, updatedUser, option);
+    res.send(result);
+})
 }
 run()
 
